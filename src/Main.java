097c10f0778ac91bb5dd58fb1c1bcd0fdb2f96f5
eesaulova <katarinaes61@gmail.com>
2022-07-21 01:19:12 +0300

    Change ticketTotalCost to count bonus miles

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
-
-        int ticketTotalCost = 1111; //общая стоимость билета
+        int ticketTotalCost = 1113; //общая стоимость билета
         int bonus = 20; //количество рублей, за которые начисляется 1 миля
 
         System.out.println("Будет начислено:");
         System.out.println((ticketTotalCost/bonus) + " бонусных миль");
-
     }
 }
