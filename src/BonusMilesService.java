445c619884219100e448c189ecff2279d4e52412
eesaulova <katarinaes61@gmail.com>
2022-08-08 02:01:27 +0300

    Изменила описание ридми файла

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int cost) { //общая стоимость билета
-        int amountForBonus = 20; //количество рублей, за которые начисляется 1 миля
-        int bonus = cost / amountForBonus; //Формула расчета миль
+    public int calculate(int cost) { // общая стоимость билета
+        int amountForBonus = 20; // количество руб., за которые начисляется 1 миля
+        int bonus = cost / amountForBonus; // формула расчета миль
 
         return bonus; // возвращаем рассчитанный bonus
     }
