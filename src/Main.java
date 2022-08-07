dd9325bb4dd188359b5d9eee9278362356d2fc4c
eesaulova <katarinaes61@gmail.com>
2022-08-08 00:37:46 +0300

    1. Добавила класс BonusMilesService и вынесла расчет в функцию calculate. 2. Описала вызов функции из нового класса в файле Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketTotalCost = 1113; //общая стоимость билета
-        int bonus = 20; //количество рублей, за которые начисляется 1 миля
-
-        System.out.println("Будет начислено:");
-        System.out.println((ticketTotalCost/bonus) + " бонусных миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
