a963a27b29feed2cc5f86ca9a29a6e1c29f9de86
eesaulova <katarinaes61@gmail.com>
2022-08-15 21:07:11 +0300

    Изменила описание ридми файла

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
+
+        int ticketTotalCost = 1111; //общая стоимость билета
+        int bonus = 20; //количество рублей, за которые начисляется 1 миля
+
+        System.out.println("Будет начислено:");
+        System.out.println((ticketTotalCost/bonus) + " бонусных миль");
+
     }
 }
